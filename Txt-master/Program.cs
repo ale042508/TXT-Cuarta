8c5442825b6dfd8441e5f7c2a89ec12ddd3b2c70
alejandrorosa25 <109633214+alejandrorosa25@users.noreply.github.com>
2024-03-03 12:36:32 -0400

    Toda la carpeta

---
+++ Txt-master/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TXT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contenido = "Este es un archivo de texto generado por un programa en C#.";
+
+            try
+            {
+                string rutaArchivo = @"C:\\Users\\Orozco\\Desktop\\mi_archivo.txt";
+
+                using (StreamWriter archivo = new StreamWriter(rutaArchivo))
+                {
+                    archivo.WriteLine(contenido);
+                }
+
+                Console.WriteLine("Archivo generado exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al generar el archivo: " + ex.Message);
+            }
+        }
+    }
+}
